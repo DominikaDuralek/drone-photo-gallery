6e3d8847fe213e88bccc803e8a07242c6b7e36d3
DominikaDuralek <dominikaduralek@gmail.com>
2024-05-24 00:04:50 +0200

    Working on the website layout.

---
+++ js/main.js
@@ -0,0 +1,8 @@
+import '../css/modern-normalize.css';
+import '../css/style.css';
+import '../css/components/header.css';
+import '../css/components/hero.css';
+import '../css/components/gallery.css';
+import '../css/components/contact.css';
+import '../css/components/footer.css';
+import '../css/utils.css';
