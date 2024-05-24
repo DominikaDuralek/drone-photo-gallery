be7889a84da8ec7a0b6ee8a7cbe12992144a98a4
DominikaDuralek <dominikaduralek@gmail.com>
2024-05-24 23:50:02 +0200

    Developing the UI

--- js/main.js
+++ js/main.js
@@ -6,3 +6,20 @@ import '../css/components/gallery.css';
 import '../css/components/contact.css';
 import '../css/components/footer.css';
 import '../css/utils.css';
+
+window.onload = function () {
+    document.body.style.visibility = 'visible';
+}
+
+function expandPhoto(imgElement) {
+  const expandedPhotoContainer = document.getElementById('expandedPhotoContainer');
+  const expandedPhoto = document.getElementById('expandedPhoto');
+  
+  expandedPhoto.src = imgElement.src;
+  expandedPhotoContainer.classList.add('active');
+}
+
+function collapsePhoto() {
+  const expandedPhotoContainer = document.getElementById('expandedPhotoContainer');
+  expandedPhotoContainer.classList.remove('active');
+}
